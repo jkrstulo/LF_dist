20a12dfb7a3b0da015b05b93579a7fdf6589a8ac
jko <jakov.krstulovic@gmail.com>
2017-03-09 14:46:01 +0100

    PF options imported explicitly from function parameters, pypower.poption() is not used any more
    enforce_qlim option is updated to be more readable and it is testend on simple_example()

--- run_bfswpf.py
+++ run_bfswpf.py
@@ -1,13 +1,12 @@
-import sys
 
 import numpy as np
 import scipy as sp
 import networkx as nx
 
-from time import time
-# import timeit.default_timer as time
+from time import time  # alternatively use import timeit.default_timer as time
 
-from scipy.sparse import issparse, csr_matrix
+
+from scipy.sparse import csr_matrix
 
 from pypower.idx_bus import BUS_I, BUS_TYPE, PD, QD, VM, VA, REF, GS, BS
 from pypower.idx_brch import F_BUS, T_BUS, BR_R, BR_X, BR_B, PF, QF, PT, QT, TAP, BR_STATUS
@@ -16,16 +15,15 @@ from pypower.idx_gen import GEN_BUS, PG, QG, PMAX, PMIN, QMAX, QMIN, GEN_STATUS,
 from pandapower.pypower_extensions.makeYbus_pypower import makeYbus
 from pandapower.pypower_extensions.pfsoln import pfsoln
 from pandapower.pypower_extensions.bustypes import bustypes
-from pandapower.run import LoadflowNotConverged
 
 from pypower.makeSbus import makeSbus
-from pypower.ppoption import ppoption
+
 
 
 class ConvergenceError(Exception):
     pass
 
-
+# TODO: make an external _run_bfsw(net, **kwargs) in order to enable defining specific parameters for each PF
 
 def reindex_bus_ppc(ppc, bus_ind_dict):
     """
@@ -188,7 +186,8 @@ def bibc_bcbv(ppc):
 
 
 
-def bfsw(DLF, bus, gen, branch, baseMVA, Ybus, Sbus, V0, ref, pv, pq, ppopt=None, tol_inner=1e-2):
+def bfswpf(DLF, bus, gen, branch, baseMVA, Ybus, Sbus, V0, ref, pv, pq,
+         enforce_q_lims, tolerance_kva, max_iteration, **kwargs):
     """
     distribution power flow solution according to [1]
     :param DLF: direct-Load-Flow matrix which relates bus current injections to voltage drops from the root bus
@@ -209,14 +208,18 @@ def bfsw(DLF, bus, gen, branch, baseMVA, Ybus, Sbus, V0, ref, pv, pq, ppopt=None
     :References:
     [1] Jen-Hao Teng, "A Direct Approach for Distribution System Load Flow Solutions", IEEE Transactions on Power Delivery, vol. 18, no. 3, pp. 882-887, July 2003.
     """
-    ## options
-    if ppopt is None:
-        ppopt = ppoption()
-    tol = ppopt['PF_TOL']
-    max_it = ppopt['PF_MAX_IT_GS']  # maximum iterations from Gauss-Seidel
-    verbose = ppopt['VERBOSE']
+    # setting options
+    tolerance_mva = tolerance_kva * 1e-3
+    max_it = max_iteration  # maximum iterations
+    verbose = kwargs["VERBOSE"]     # verbose is set in run._runpppf() #
+    # tolerance for the inner loop for PV nodes
+    if 'tolerance_kva_pv' in kwargs:
+        tol_mva_inner = kwargs['tolerance_kva_pv'] * 1e-3
+    else:
+        tol_mva_inner = 1.e-2
 
     nbus = bus.shape[0]
+    ngen = gen.shape[0]
 
     mask_root = ~ (bus[:, BUS_TYPE] == 3)  # mask for eliminating root bus
     root_bus_i = ref
@@ -224,9 +227,6 @@ def bfsw(DLF, bus, gen, branch, baseMVA, Ybus, Sbus, V0, ref, pv, pq, ppopt=None
 
     bus_ind_mask_dict = dict(zip(bus[mask_root, BUS_I], range(nbus - 1)))
 
-    # detect PV buses
-    busPV_ind_mask = [bus_ind_mask_dict[pvbus] for pvbus in pv]
-    genPV_ind = np.where(np.in1d(gen[:, GEN_BUS], pv))[0].flatten()
 
     # compute shunt admittance
     # if Psh is the real power consumed by the shunt at V = 1.0 p.u. and Qsh is the reactive power injected by
@@ -243,12 +243,19 @@ def bfsw(DLF, bus, gen, branch, baseMVA, Ybus, Sbus, V0, ref, pv, pq, ppopt=None
 
     Ysh += (Gch_f + Gch_t) + 1j * (Bch_f + Bch_t)  # adding line charging to shunt impedance vector
 
+    # detect generators on PV buses which have status ON
+    gen_pv = np.in1d(gen[:, GEN_BUS], pv) & (gen[:, GEN_STATUS] > 0)
+    qg_lim = np.zeros(ngen, dtype=bool)   #initialize generators which violated Q limits
+
     V_iter = V0[mask_root].copy()  # initial voltage vector without root bus
     V = V0.copy()
     Iinj = np.conj(Sbus[mask_root] / V_iter) - Ysh[mask_root] * V_iter  # Initial current injections
 
     n_iter = 0
     converged = 0
+
+    if verbose:
+        print(' -- AC Power Flow Backward/Forward sweep\n')
 
     while not converged and n_iter < max_it:
         n_iter_inner = 0
@@ -264,36 +271,57 @@ def bfsw(DLF, bus, gen, branch, baseMVA, Ybus, Sbus, V0, ref, pv, pq, ppopt=None
 
         success_inner = 1
         while not inner_loop_converged and len(pv) > 0:
-            Vmis = (np.abs(gen[genPV_ind, VG])) ** 2 - (np.abs(V_inner[busPV_ind_mask])) ** 2
-            dQ = (Vmis / (2 * DLF[busPV_ind_mask, busPV_ind_mask].A1.imag)).flatten()
 
-            gen[genPV_ind, QG] += dQ
-            if (gen[genPV_ind, QG] < gen[genPV_ind, QMIN]).any():
-                Qviol_ind = np.argwhere((gen[genPV_ind, QG] < gen[genPV_ind, QMIN])).flatten()
-                gen[genPV_ind[Qviol_ind], QG] = gen[genPV_ind[Qviol_ind], QMIN]
-            elif (gen[genPV_ind, QG] > gen[genPV_ind, QMAX]).any():
-                Qviol_ind = np.argwhere((gen[genPV_ind, QG] < gen[genPV_ind, QMIN])).flatten()
-                gen[genPV_ind[Qviol_ind], QG] = gen[genPV_ind[Qviol_ind], QMAX]
+            pvi = pv - 1  # internal PV buses indices, assuming reference node is always 0
+
+            Vmis = (np.abs(gen[gen_pv, VG])) ** 2 - (np.abs(V_inner[pvi])) ** 2
+            dQ = (Vmis / (2 * DLF[pvi, pvi].A1.imag)).flatten()
+
+            gen[gen_pv, QG] += dQ
+
+            if enforce_q_lims:  #check Q violation limits
+                ## find gens with violated Q constraints
+                qg_max_lim = (gen[:, QG] > gen[:, QMAX]) & gen_pv
+                qg_min_lim = (gen[:, QG] < gen[:, QMIN]) & gen_pv
+
+                if qg_min_lim.any():
+                    gen[qg_min_lim, QG] = gen[qg_min_lim, QMIN]
+                    bus[gen[qg_min_lim, GEN_BUS].astype(int), BUS_TYPE] = 1 # convert to PQ bus
+
+                if qg_max_lim.any():
+                    gen[qg_max_lim, QG] = gen[qg_max_lim, QMAX]
+                    bus[gen[qg_max_lim, GEN_BUS].astype(int), BUS_TYPE] = 1  # convert to PQ bus
+
+                # TODO: correct: once all the PV buses are converted to PQ buses, conversion back to PV is not possible
+                qg_lim_new = qg_min_lim | qg_max_lim
+                if qg_lim_new.any():
+                    pq2pv = (qg_lim != qg_lim_new) &  qg_lim
+                    # convert PQ to PV bus
+                    if pq2pv.any():
+                        bus[gen[qg_max_lim, GEN_BUS].astype(int), BUS_TYPE] = 2  # convert to PV bus
+
+                    qg_lim = qg_lim_new.copy()
+                    ref, pv, pq = bustypes(bus, gen)
+
 
             Sbus = makeSbus(baseMVA, bus, gen)
             Iinj = np.conj(Sbus[mask_root] / V_inner) - Ysh[mask_root] * V_inner
             deltaV = DLF * Iinj
             V_inner = np.ones(nbus - 1) * V0[root_bus_i] + deltaV
 
-            if n_iter_inner > 20 or np.any(np.abs(V_inner[busPV_ind_mask]) > 2):
+            if n_iter_inner > 20 or np.any(np.abs(V_inner[pvi]) > 2):
                 success_inner = 0
                 break   # TODO: special notice for divergence due to inner iterations for PV nodes
 
 
             n_iter_inner += 1
 
-            if np.all(np.abs(dQ) < tol_inner):  # inner loop termination criterion
+            if np.all(np.abs(dQ) < tol_mva_inner):  # inner loop termination criterion
                 inner_loop_converged = True
                 V_new = V_inner.copy()
 
         if not success_inner:
-            if verbose:
-                break
+            break
 
         # testing termination criterion -
         V = np.insert(V_new, root_bus_i, Vref)
@@ -307,7 +335,7 @@ def bfsw(DLF, bus, gen, branch, baseMVA, Ybus, Sbus, V0, ref, pv, pq, ppopt=None
 
         # deltaVmax = np.max(np.abs(V_new - V_iter))
 
-        if normF < tol:
+        if normF < tolerance_mva:
             converged = 1
             if verbose:
                 print("\nFwd-back sweep power flow converged in "
@@ -324,7 +352,7 @@ def bfsw(DLF, bus, gen, branch, baseMVA, Ybus, Sbus, V0, ref, pv, pq, ppopt=None
 
 
 
-def _run_bfsw(ppc, ppopt=None):
+def _run_bfswpf(ppc, enforce_q_lims, tolerance_kva, max_iteration, **kwargs):
     """
     SPARSE version of distribution power flow solution according to [1]
     :References:
@@ -334,8 +362,6 @@ def _run_bfsw(ppc, ppopt=None):
     :return: results (pypower style), success (flag about PF convergence)
     """
     ppci = ppc
-
-    ppopt = ppoption(ppopt)
 
     baseMVA, bus, gen, branch = \
         ppci["baseMVA"], ppci["bus"], ppci["gen"], ppci["branch"]
@@ -353,7 +379,7 @@ def _run_bfsw(ppc, ppopt=None):
 
     time_start = time() # starting pf calculation timing
 
-    # initialize voltages to flat start and buses with gens to their setpoints
+    # initialize voltages to flat start and buses with gens to their setpoint voltage magnitudes
     V0 = np.ones(nbus, dtype=complex)
     V0[gen[:, GEN_BUS].astype(int)] = gen[:, VG]
 
@@ -365,8 +391,8 @@ def _run_bfsw(ppc, ppopt=None):
     ref_bfsw, pv_bfsw, pq_bfsw = bustypes(bus_bfsw, gen_bfsw)
 
     # #-----  run the power flow  -----
-    V_final, success = bfsw(DLF, bus_bfsw, gen_bfsw, branch_bfsw, baseMVA_bfsw, Ybus_bfsw, Sbus_bfsw, V0,
-                                   ref_bfsw, pv_bfsw, pq_bfsw, ppopt=ppopt)
+    V_final, success = bfswpf(DLF, bus_bfsw, gen_bfsw, branch_bfsw, baseMVA_bfsw, Ybus_bfsw, Sbus_bfsw, V0,
+                                   ref_bfsw, pv_bfsw, pq_bfsw, enforce_q_lims, tolerance_kva, max_iteration, **kwargs)
 
     V_final = V_final[np.argsort(buses_ordered_bfsw)]  # return bus voltages in original bus order
 
